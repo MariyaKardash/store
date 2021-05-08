51a6e5edf69c7856c4405219f740cf0b954ca1b3
Mariya Kardash <mariya.kardash2@gmail.com>
2021-05-08 11:03:07 +0300

    access with product in db

---
+++ backend/routers/productRouter.js
@@ -0,0 +1,23 @@
+import express from "express";
+import expressAsyncHandler from "express-async-handler";
+import Product from "../models/productModel";
+
+const productRouter = express.Router();
+
+productRouter.get(
+  "/",
+  expressAsyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.send(products);
+  })
+);
+
+productRouter.get(
+  "/:id",
+  expressAsyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    res.send(product);
+  })
+);
+
+export default productRouter;
--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,18 @@
 import express from 'express';
 import cors from 'cors';
-import data from './data';
 import mongoose from 'mongoose';
 import userRouter from './routers/userRouter';
 import orderRouter from './routers/orderRouter';
+import productRouter from './routers/productRouter'
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.get('/api/products', (req, res) => {
-  res.send(data.products);
-});
 
+app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
-
-app.get('/api/products/:id', (req, res) => {
-  const product = data.products.find(x => x._id === req.params.id);
-  if(product) {
-    res.send(product);
-  } else {
-    res.status(404).send({message: 'Данный товар сейчас отсутствует на складе!'});
-  }
-})
 
 app.use((err, req, res, next) => {
   const status = err.name && err.name === 'ValidationError'?400:500;
